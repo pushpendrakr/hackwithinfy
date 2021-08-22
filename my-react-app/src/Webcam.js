c063986c0a89877d44b41b48c26f09d075031be3
pushpendrakr <52634462+pushpendrakr@users.noreply.github.com>
2021-08-22 09:13:09 +0530

    code

--- my-react-app/src/Webcam.js
+++ my-react-app/src/Webcam.js
@@ -17,25 +17,36 @@ const videoConstraints = {
     }, [webcamRef, setImgSrc]);
   
     return (
-      <>
-      
+      <div className="row">
+       <div className="col">
+         <div>
         <Webcam
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
           width="400"
           height="400"
-        />
-       
-        <button className='btn-btn-primary' onClick={capture}>Capture photo</button>
-        <div>
-        {imgSrc && (
+        /></div>
+      
+        <button margin-left='8em' className='btn-btn-primary' onClick={capture}>Capture photo</button>
+        </div>
+        
+        <div className="col">
+        {imgSrc && (<div>
           <img display='block'
             src={imgSrc}
           />
+          <div align='center'>Age:21</div>
+        <div align='center'>Gender:Male</div></div>
         )}
+        
         </div>
-      </>
+        <div className='col'>
+          
+        </div>
+        </div>
+      
+    
     );
   };
   
